b8dd20e093ae539699dc2a0ec48fb134b8d0ae2c
wangjiequn <wangjiequn@qq.com>
2016-02-27 08:49:30 +0800

    aihu

---
+++ aihu_core/src/main/java/com/team/log/JayChumLogger.java
@@ -0,0 +1,26 @@
+package com.team.log;
+
+public abstract interface JayChumLogger
+{
+  public abstract void biz(String paramString);
+
+  public abstract void debug(String paramString);
+
+  public abstract void info(String paramString);
+
+  public abstract void error(String paramString);
+
+  public abstract void warn(String paramString);
+
+  public abstract void fatal(String paramString);
+
+  public abstract void debug(String paramString, Throwable paramThrowable);
+
+  public abstract void info(String paramString, Throwable paramThrowable);
+
+  public abstract void error(String paramString, Throwable paramThrowable);
+
+  public abstract void warn(String paramString, Throwable paramThrowable);
+
+  public abstract void fatal(String paramString, Throwable paramThrowable);
+}
